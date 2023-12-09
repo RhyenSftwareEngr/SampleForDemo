227b362fe7440899a00bcabe48263dfc3dfbdc35
Shanthal <2227297@slu.edu.ph>
2023-12-09 14:47:23 +0800

    Fix: Updated AddNumbers Method

--- Main.java
+++ Main.java
@@ -1,14 +1,12 @@
-package com.example;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 
+    // Updated addNumbers method
     public int addNumbers(int a, int b) {
+        // Include a log statement
+        System.out.println("Adding numbers: " + a + " + " + b);
         return a + b;
     }
-    public int multiplyNumbers(int a, int b) {
-    return a * b;
-}
 }
